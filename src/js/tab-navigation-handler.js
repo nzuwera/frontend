dbf5107d5353eca978de23c170f93a696e60010d
nzuwera <gilbert.nzuwera@telpaygroup.com>
2024-10-20 17:37:27 +0200

    chore: UI - re-design
    - [x] Hide content before login
    - [x] Display content after login

--- src/js/tab-navigation-handler.js
+++ src/js/tab-navigation-handler.js
@@ -1,4 +1,16 @@
 const triggerTabList = document.querySelectorAll('#userTabs a')
+const loginBtn = document.getElementById("login-btn");
+const contentWrapper = document.getElementById("content-wrapper");
+
+// TODO to be replaced with login form handling
+loginBtn.addEventListener('click',function (event) {
+    event.preventDefault();
+    // contentWrapper.removeClass('d-none');
+    if (contentWrapper.classList.contains('d-none')) {
+        contentWrapper.classList.remove('d-none');
+        // alert("remove faq display!");
+    }
+})
 triggerTabList.forEach(triggerEl => {
     const tabTrigger = new bootstrap.Tab(triggerEl)
 
